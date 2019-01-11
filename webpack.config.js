50db6d6d45031b91428f43e79fc05a959e046c4d
Oriol Ylla <uriylla@gmail.com>
2019-01-11 01:26:00 +0100

    Updated babel dependencies. Reorganize dev dependencies. sass loader configured

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,16 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node-modules/
+    },{
+      test: /\.scss$/,
+      use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+      ]
     }]
   },
-  devtool: 'cheap-module-eval-source-map',
+  // devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public')
   },
