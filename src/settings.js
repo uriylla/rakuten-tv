88dc6675b1bd9ccc205374576dbd297e1e830653
Oriol Ylla <uriylla@gmail.com>
2019-01-11 03:00:18 +0100

    settings file created

---
+++ src/settings.js
@@ -0,0 +1,13 @@
+export default {
+  'API_BASE_URL': 'https://gizmo.rakuten.tv/v3/',
+  'API_QUERY_PARAMS': '?classification_id=5&device_identifier=web&locale=es&market_code=es',
+  'lists': [
+    'populares-en-taquilla',
+    'estrenos-para-toda-la-familia',
+    'estrenos-imprescindibles-en-taquilla',
+    'estrenos-espanoles',
+    'si-te-perdiste',
+    'especial-x-men',
+    'nuestras-preferidas-de-la-semana'
+  ]
+}
