9c56b7916e23e69c8e04a1ee000a3385bd810280
Oriol Ylla <uriylla@gmail.com>
2019-01-14 01:55:15 +0100

    webpack config: unhide performance hints, enable source mapping

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,8 @@ module.exports = {
       ]
     }]
   },
-  // devtool: 'cheap-module-eval-source-map',
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public')
-  },
-  performance: {
-    hints: false
   }
 };
