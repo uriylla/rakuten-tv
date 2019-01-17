cbcc608e71982f47c37f76ad6dd3b26947555736
Oriol Ylla <uriylla@gmail.com>
2019-01-17 01:05:18 +0100

    configure production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const path = require('path');
 
-module.exports = {
-  entry: ["@babel/polyfill", './src/app.js'],
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename : 'bundle.js',
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node-modules/
-    },{
-      test: /\.scss$/,
-      use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: ["@babel/polyfill", './src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename : 'bundle.js',
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node-modules/
+      },{
+        test: /\.scss$/,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+        ]
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    mode: isProduction ? 'production' : 'development',
+    performance: { hints: false },
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
+    }
   }
-};
+}
