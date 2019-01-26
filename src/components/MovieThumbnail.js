0cb1728d2332f996bc5ad2b4b496bf8ae7f014cd
Oriol Ylla <uriylla@gmail.com>
2019-01-26 15:47:57 +0100

    header and movie thumbnail styling. Adding score info to movie thumbnail

--- src/components/MovieThumbnail.js
+++ src/components/MovieThumbnail.js
@@ -2,10 +2,28 @@ import React from 'react';
 
 export default class MovieThumbnail extends React.Component {
   render () {
-    const { artwork_url, score } = this.props
+    const { artwork_url, highlighted_score } = this.props
+    const { formatted_amount_of_votes, score } = highlighted_score
     return (
-      <div className="movie">
-        <img className="movie__image" src={artwork_url}/>
+      <div className="thumbnail">
+        <div className="thumbnail__cover">
+          <div className="thumbnail__border"></div>
+          <img className="thumbnail__image" src={artwork_url}/>
+        </div>
+        <div className="thumbnail__info">
+          <div className="thumbnail__votes">
+            <span >
+              <span className="material-icons">star</span>
+              {formatted_amount_of_votes}
+            </span>
+          </div>
+          <div className="thumbnail__score">
+            <span>
+              <span className="material-icons">person</span>
+              {score}
+            </span>
+          </div>
+        </div>
       </div>
     )
   }
--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -9,7 +9,7 @@ export default (state = {}, action) => {
         lists
       }
       break;
-    default: 
+    default:
       return state;
   }
 }
