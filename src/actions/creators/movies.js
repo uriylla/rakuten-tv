fc7d5aed47b18610473e662b85af8f0aeb917e83
Oriol Ylla <uriylla@gmail.com>
2019-01-26 17:35:38 +0100

    select, unselect redux actions

--- src/actions/creators/movies.js
+++ src/actions/creators/movies.js
@@ -1,4 +1,4 @@
-import {  REQUEST_MOVIES_LISTS,  RECIEVE_MOVIES_LISTS } from '../types/movies'
+import {  REQUEST_MOVIES_LISTS,  RECIEVE_MOVIES_LISTS, SELECT_MOVIE, UNSELECT_MOVIE } from '../types/movies'
 
 export const requestMoviesLists = (lists) => ({
   type:  REQUEST_MOVIES_LISTS,
@@ -8,4 +8,14 @@ export const requestMoviesLists = (lists) => ({
 export const recieveMoviesLists = (lists) => ({
   type: RECIEVE_MOVIES_LISTS,
   lists: lists
+})
+
+export const selectMovie = (id, name) => ({
+  type: SELECT_MOVIE,
+  id,
+  name
+})
+
+export const unselectMovie = () => ({
+  type: UNSELECT_MOVIE
 })
--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -1,4 +1,4 @@
-import { RECIEVE_MOVIES_LISTS } from '../actions/types/movies'
+import { RECIEVE_MOVIES_LISTS, SELECT_MOVIE, UNSELECT_MOVIE } from '../actions/types/movies'
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -7,6 +7,20 @@ export default (state = {}, action) => {
       return {
         ...state,
         lists
+      }
+    case SELECT_MOVIE:
+      return {
+        ...state,
+        selectedMovie: {
+          id: action.id,
+          name: action.name
+        }
+      }
+      break;
+    case UNSELECT_MOVIE:
+      return {
+        ...state,
+        selectedMovie: undefined
       }
       break;
     default:
