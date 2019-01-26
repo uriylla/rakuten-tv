858f87c52b55cb3e9937650477c298553c4a3d4f
Oriol Ylla <uriylla@gmail.com>
2019-01-26 17:35:54 +0100

    selected movie selectors

--- src/selectors/movies.js
+++ src/selectors/movies.js
@@ -1,3 +1,7 @@
 export const selectLists = (lists) => lists && lists.filter(list => !!list)
 
 export const selectListById = (lists, id) => lists.find(list => list.id === id)
+
+export const getSelectedMovie = ({selectedMovie}) => selectedMovie
+
+export const getSelectedMovieName = ({selectedMovie}) => selectedMovie ? selectedMovie.name : undefined
